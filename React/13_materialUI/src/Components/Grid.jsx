3eed381012af70ac64c45ee74185ee7679c23616
Sahil Murtuza <sahilmurtuza.it@gmail.com>
2026-04-02 13:01:52 +0530

    matrialUI Grid imporve

--- React/13_materialUI/src/Components/Grid.jsx
+++ React/13_materialUI/src/Components/Grid.jsx
@@ -1,35 +1,30 @@
-import React from 'react'
-import { Box, Grid } from '@mui/material'
+import React from "react";
+import { Box, Grid } from "@mui/material";
 function MyGrid() {
   return (
     <div>
-
       <Grid container spacing={2}>
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <Box sx={{ p: 2, border: '1px solid black' }}>Block 1</Box>
+        <Grid item size={{xs:12, sm:6, md:4, lg:3}} >
+          <Box sx={{ p: 2, border: "1px solid black" }}>Block 1</Box>
         </Grid>
 
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <Box sx={{ p: 2, border: '1px solid black' }}>Block 2</Box>
+        <Grid item size={{xs:12, sm:6, md:4, lg:3}}>
+          <Box sx={{ p: 2, border: "1px solid black" }}>Block 2</Box>
         </Grid>
 
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <Box sx={{ p: 2, border: '1px solid black' }}>Block 3</Box>
+        <Grid item size={{xs:12, sm:6, md:4, lg:3}}>
+          <Box sx={{ p: 2, border: "1px solid black" }}>Block 3</Box>
         </Grid>
 
-        <Grid item xs={12} sm={6} md={4} lg={3}>
-          <Box sx={{ p: 2, border: '1px solid black' }}>Block 4</Box>
+        <Grid item size={{xs:12, sm:6, md:4, lg:3}}>
+          <Box sx={{ p: 2, border: "1px solid black" }}>Block 4</Box>
         </Grid>
       </Grid>
-
-
-
-
     </div>
-  )
+  );
 }
 
-export default MyGrid
+export default MyGrid;
 
 // xs-->extra small (mobile)
 // sm--> small (tablet)
